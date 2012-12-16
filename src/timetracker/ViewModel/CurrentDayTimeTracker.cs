4c6cffd7e7e25c8b3af7dac82f7bd3625b868e28
Nick Beer <Beer.Nick@gmail.com>
2012-12-16 13:36:44 -0600

    Add support for day navigation

--- src/timetracker/ViewModel/CurrentDayTimeTracker.cs
+++ src/timetracker/ViewModel/CurrentDayTimeTracker.cs
@@ -12,6 +12,23 @@ namespace Beerman006.TimeTracker.ViewModel
     [Export]
     public class CurrentDayTimeTracker : ViewModelBase
     {
+        #region Fields
+        /// <summary>
+        /// The current <see cref="TimedDay"/>.
+        /// </summary>
+        private TimedDay _currentDay;
+
+        /// <summary>
+        /// The current date.
+        /// </summary>
+        private DateTime _currentDate;
+
+        /// <summary>
+        /// The view model for time entry.
+        /// </summary>
+        private TimeEntryViewModel _timeEntryViewModel;
+        #endregion
+
         #region Constructor
         /// <summary>
         /// Constructs a new <see cref="CurrentDayTimeTracker"/>.
@@ -23,7 +40,7 @@ namespace Beerman006.TimeTracker.ViewModel
             TimeEntryManager = timeEntryManager;
 
             CurrentDate = DateTime.Now;
-            CurrentTimeEntry = new TimeEntryViewModel(clientManager, CurrentDate);
+            TimeEntryViewModel = new TimeEntryViewModel(clientManager, CurrentDate);
         }
         #endregion
 
@@ -41,22 +58,42 @@ namespace Beerman006.TimeTracker.ViewModel
         /// <summary>
         /// Gets the current date.
         /// </summary>
-        public DateTime CurrentDate { get; private set; }
+        public DateTime CurrentDate 
+        {
+            get { return _currentDate; }
+            private set
+            {
+                SetProperty("CurrentDate", ref _currentDate, value);
+                CurrentDay = TimeEntryManager[CurrentDate];
+                TimeEntryViewModel = new TimeEntryViewModel(ClientManager, CurrentDate);
+            }
+        }
 
         /// <summary>
-        /// Gets all the <see cref="TimeEntry"/>s that have been added for today.
+        /// Gets all the <see cref="TimeEntryViewModel"/>s that have been added for today.
         /// </summary>
         public IEnumerable<TimeEntry> TimeEntries { get { return CurrentDay.TimeEntries; } }
 
         /// <summary>
         /// Gets the <see cref="TimedDay"/> for the current day.
         /// </summary>
-        public TimedDay CurrentDay { get { return TimeEntryManager[CurrentDate]; } }
+        public TimedDay CurrentDay 
+        {
+            get { return _currentDay; }
+            set { SetProperty("CurrentDay", ref _currentDay, value); }
+        }
 
         /// <summary>
         /// Gets the current <see cref="TimeEntryViewModel"/>.
         /// </summary>
-        public TimeEntryViewModel CurrentTimeEntry { get; private set; }
+        public TimeEntryViewModel TimeEntryViewModel 
+        {
+            get { return _timeEntryViewModel; }
+            private set
+            {
+                SetProperty("TimeEntryViewModel", ref _timeEntryViewModel, value);
+            }
+        }
         #endregion
 
         #region Private Methods
@@ -67,7 +104,27 @@ namespace Beerman006.TimeTracker.ViewModel
         /// <param name="e"></param>
         private void OnAddNewTimeEntryExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            CurrentDay.AddTimeEntry(CurrentTimeEntry.TimeEntry);
+            CurrentDay.AddTimeEntry(TimeEntryViewModel.TimeEntry);
+        }
+
+        /// <summary>
+        /// Called when we are instructed to go to the previous day.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnGoToPreviousDayExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            CurrentDate = CurrentDate.Subtract(TimeSpan.FromDays(1));
+        }
+
+        /// <summary>
+        /// Called when we are instructed to go to the next day.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnGoToNextDayExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            CurrentDate = CurrentDate.AddDays(1);
         }
 
         /// <summary>
@@ -78,9 +135,9 @@ namespace Beerman006.TimeTracker.ViewModel
         /// <param name="e"></param>
         private void OnCanExecuteAddNewTimeEntry(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = !string.IsNullOrEmpty(CurrentTimeEntry.ClientName) &&
-                           !string.IsNullOrEmpty(CurrentTimeEntry.WorkType) &&
-                           CurrentTimeEntry.TotalTime > TimeSpan.FromMinutes(1d);
+            e.CanExecute = !string.IsNullOrEmpty(TimeEntryViewModel.ClientName) &&
+                           !string.IsNullOrEmpty(TimeEntryViewModel.WorkType) &&
+                           TimeEntryViewModel.TotalTime > TimeSpan.FromMinutes(1d);
         }
         #endregion
 
@@ -94,7 +151,9 @@ namespace Beerman006.TimeTracker.ViewModel
             {
                 return base.CommandBindings.Concat(new[]
                 {
-                    new CommandBinding(TimeTrackerCommands.AddNewTimeEntryCommand, OnAddNewTimeEntryExecuted, OnCanExecuteAddNewTimeEntry)
+                    new CommandBinding(TimeTrackerCommands.AddNewTimeEntryCommand, OnAddNewTimeEntryExecuted, OnCanExecuteAddNewTimeEntry),
+                    new CommandBinding(TimeTrackerCommands.GoToPreviousDayCommand, OnGoToPreviousDayExecuted),
+                    new CommandBinding(TimeTrackerCommands.GoToNextDayCommand, OnGoToNextDayExecuted)
                 });
             }
         }
--- src/timetracker/ViewModel/TimeTrackerCommands.cs
+++ src/timetracker/ViewModel/TimeTrackerCommands.cs
@@ -16,5 +16,23 @@ namespace Beerman006.TimeTracker.ViewModel
                 "Add Time Entry",
                 "TimeTrackerCommands.AddNewTimeEntryCommand",
                 typeof(TimeTrackerCommands));
+
+        /// <summary>
+        /// A command for navigating to the next day's time entry.
+        /// </summary>
+        public static readonly RoutedUICommand GoToNextDayCommand =
+            new RoutedUICommand(
+                "Go To Next Day",
+                "TimeTrackerCommands.GoToNextDayCommand",
+                typeof(TimeTrackerCommands));
+
+        /// <summary>
+        /// A command for navigating to the previous day's time entry.
+        /// </summary>
+        public static readonly RoutedUICommand GoToPreviousDayCommand =
+            new RoutedUICommand(
+                "Go To Previous Day",
+                "TimeTrackerCommands.GoToPreviousDayCommand",
+                typeof(TimeTrackerCommands));
     }
 }
